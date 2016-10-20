84e0a9368f97d855e21f6fea482e79a31088696e
JunYoung Kim <kimjun136@outlook.com>
2016-10-20 12:09:57 +0900

    create users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name', 30);
+            $table->string('email', 200);
+	    $table->string('phone', 20);
+	    $table->string('user_id', 30)->unique();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
