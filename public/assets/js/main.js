c6385d90b05f7610be894997716435d3bfd2fd9e
m4stem4 <m4stem4@naver.com>
2016-10-31 00:59:01 +0900

    Main page design

---
+++ public/assets/js/main.js
@@ -0,0 +1,10 @@
+/*function heightResize() {
+	var mainPage = document.getElementById('test');
+	mainpage.style.padding = "";
+	//mainPage.style.margin = "0 -5px 0 -5px";
+}
+window.onload = function() {
+	heightResize();
+	window.addEventListener('resize', heightResize);
+}
+*/
