2fa7a5192cb34b1ace112521d136e50c06fdad77
m4stem4 <m4stem4@naver.com>
2016-11-03 21:00:02 +0900

    Main page design & make search function

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -29,3 +29,26 @@ function offFocus() {
 	//search.innerHTML = "<input type='text' id='dataSearch' onfocus='onFocus(); onfocusout='offFocus()'/><div class='glyphicon glyphicon-search' onclick='offSearch()'></div>";
 }
 */
+//$('#searchlist').hide();
+
+
+function search() {
+	var search = document.getElementById('search');
+	search.innerHTML = "<input type='text' id='dataSearch' placeholder='검색하고 싶은 팀명을 입력하세요.'/><li id='inputform' class='menu' onclick='desearch();''><a href='javascript:void(0)'><img src='assets/img/search.png' alt='' /></a></li>";
+	$('#dataSearch').focus(function() {
+		focus();
+	});
+	$('#dataSearch').focusout(function() {
+		outfocus();
+	});
+}
+function desearch() {
+	var search = document.getElementById('search');
+	search.innerHTML = "<li id='inputform' class='menu' onclick='search();''><a href='javascript:void(0)'><img src='assets/img/search.png' alt='' /></a></li>";
+}
+function focus() {
+	$('#searchlist').css("display", "block");
+}
+function outfocus() {
+	$('#searchlist').css("display", "none");
+}
