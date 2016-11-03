94001ce305d3242af8220a6415c1dfa2c4958cfd
m4stem4 <m4stem4@naver.com>
2016-11-03 09:27:35 +0900

    main page refactoring

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -7,4 +7,25 @@ window.onload = function() {
 	heightResize();
 	window.addEventListener('resize', heightResize);
 }
+
+window.onload = function() {
+	window.addEventListener('onclick', onSearch);
+}
+function onSearch() {
+	var test = document.getElementById('test');
+	test.innerHTML = "<input type='text' id='dataSearch' onfocus='onFocus();'/><a href='javascript:void(0);'><div class='glyphicon glyphicon-search' onclick='offSearch()'></div></a>";
+}
+function offSearch() {
+	var test = document.getElementById('test');
+	test.innerHTML = "<a href='javascript:void(0)'' id='search'><div class='glyphicon glyphicon-search' onclick='onSearch()''></div></a>";
+
+}
+function onFocus() {
+	var focus = document.getElementById('dataSearch');
+	//search.innerHTML = "<input type='text' id='dataSearch' onfocus='onFocus(); onfocusout='offFocus()'/><div class='glyphicon glyphicon-search' onclick='offSearch()'></div><div id='dataList'></div>";
+}
+function offFocus() {
+	var focus = document.getElementById('dataSearch');
+	//search.innerHTML = "<input type='text' id='dataSearch' onfocus='onFocus(); onfocusout='offFocus()'/><div class='glyphicon glyphicon-search' onclick='offSearch()'></div>";
+}
 */
