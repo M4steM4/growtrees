0336f68d06ce9508fb508d67a6ec26fad72d9d10
m4stem4 <m4stem4@naver.com>
2016-11-04 18:22:55 +0900

    Chatting down

--- public/assets/js/project.js
+++ public/assets/js/project.js
@@ -22,3 +22,13 @@ function todoliston() {
 function todolistout() {
 	$('#todolist').css("display", "none");
 }
+function chattingdown() {
+	$('#chat').css("display", "none");
+	$('#waterspread').css("bottom", "0");
+	document.getElementById('waterspread').innerHTML = '<img src="assets/img/waterspread.png" width="100" height="60"  onclick="chattingup();">';
+}
+function chattingup() {
+	$('#chat').css("display", "block");
+	$('#waterspread').css("bottom", "380px");
+	document.getElementById('waterspread').innerHTML = '<img src="assets/img/waterspread.png" width="100" height="60"  onclick="chattingdown();">';
+}
