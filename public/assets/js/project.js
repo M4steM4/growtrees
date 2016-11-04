286af9345efd0a74aac4243cf856216bff125649
m4stem4 <m4stem4@naver.com>
2016-11-04 13:07:00 +0900

    Make roll section

--- public/assets/js/project.js
+++ public/assets/js/project.js
@@ -1,6 +1,18 @@
-function showTel() {
-	$('#office').css("display", "block");
+function show(x) {
+	$('#' + x).css("display", "block");
 }
-function closeTel() {
+function closeflex() {
+	$('#calender').css("display", "none");
+}
+function closetel() {
 	$('#office').css("display", "none");
+}
+function showflex(x) {
+	$('#' + x).css("display", "flex");
+}
+function rollon() {
+	$('#roll').css("display", "flex");
+}
+function rollout() {
+	$('#roll').css("display", "none");
 }
